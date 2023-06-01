87c3b9d6349343ddd570c8b9283626dcb4b6989f
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2023-06-01 14:15:23 +0200

    Models

---
+++ app/models/ExamenModel.php
@@ -0,0 +1,28 @@
+<?php
+
+class ExamenModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getExamens()
+    {
+        $sql = "SELECT CONCAT(examinator.voornaam, ' ', examinator.tussenvoegsel, ' ', examinator.achternaam) AS Naam
+                        ,examen.Datum
+                        ,examen.Rijbewijscategorie
+                        ,examen.Rijschool
+                        ,examen.Stad
+                        ,examen.Uitslag 
+        FROM examenperexaminator
+        INNER JOIN examen ON examen.Id = examenperexaminator.ExamenId
+        INNER JOIN examinator on examinator.Id = examenperexaminator.ExaminatorId;";
+
+        $this->db->query($sql);
+
+        return $this->db->resultSet();
+    }
+}
